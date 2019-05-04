ea71c91e31958a826854e0da11b6f3b1cc593902
Pavel Oborin <oborin.p@gmail.com>
2019-05-04 14:40:41 +0300

    👷 Настраиваю WSGI

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python start.py
+web: gunicorn start:app
 heroku ps:scale web=1
--- start.py
+++ start.py
@@ -5,5 +5,5 @@ from tea_site import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 33507)) 
+    port = int(os.environ.get('PORT')) 
     app.run(host='0.0.0.0', port=port, debug=False)
