e5e20d5095bbb0421f802fb2131132c8f7c17cc7
Pavel Oborin <oborin.p@gmail.com>
2019-05-04 14:32:19 +0300

    👷Поправки в CI

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
+import os
+
 from tea_site import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 33507)) 
+    app.run(host='0.0.0.0', port=port, debug=False)
