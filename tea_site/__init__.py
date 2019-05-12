dd2329874ff11ad774d6fd23b9646b0e5f4b838a
Pavel Oborin <oborin.p@gmail.com>
2019-05-13 00:40:20 +0300

    :heavy_minus_sign: Removed dependency from bcrypt

--- tea_site/__init__.py
+++ tea_site/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from tea_site.config import Config, TestConfig
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app(config=Config):
@@ -13,7 +11,6 @@ def create_app(config=Config):
     app.config.from_object(config)
 
     db.init_app(app)
-    bcrypt.init_app(app)
     login_manager.init_app(app)
 
     from tea_site.main.routes import main
