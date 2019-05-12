fc8b0ddd5dd117a50468ff83f580f69c1e8c5ef0
Pavel Oborin <oborin.p@gmail.com>
2019-05-13 01:06:29 +0300

    :sparkles: Added users forms and updated users routes
    Добавил формы для входа/регистрации
    Написал рабочие роуты для users.
    Осталось доработать шаблоны и users будут готовы.

---
+++ tea_site/users/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from tea_site.models import User
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    first_name = StringField('Имя', validators=[DataRequired()])
+    last_name = StringField('Фамилия', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(8, 64)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Эта почта уже привязанна к существующему аккаунту')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- tea_site/users/routes.py
+++ tea_site/users/routes.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import login_required, current_user, logout_user, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from tea_site import db
+from tea_site.models import User
+from tea_site.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint('users', __name__)
 
@@ -10,17 +15,41 @@ def account():
     return render_template("account.html")
 
 
-@users.route("/register")
+@users.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    first_name=form.first_name.data,
+                    last_name=form.last_name.data,
+                    password=generate_password_hash(form.password.data))
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Аккаунт был успешно создан", 'success')
+        return redirect(url_for('users.login'))
+    return render_template("register.html", title='Регистрация', form=form)
 
 
-@users.route("/login")
+@users.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash(f"Вход не удался, пожалуйста, проверьте email и пароль", 'danger')
+    return render_template("login.html", title='Вход', form=form)
 
 
 @login_required
 @users.route("/logout")
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('main.home'))
